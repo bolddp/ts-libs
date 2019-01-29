f9aca59ad16d659a1c0161a1a20605097fd98d21
Daniel Persson <bighatsmallfeet@gmail.com>
2019-01-29 20:45:47 +0100

    fixes

--- index.d.ts
+++
@@ -1,7 +0,0 @@
-import { AuthService } from './src/AuthService';
-import { AwsAuthService } from './src/AwsAuthService';
-
-declare module "auth-service" {
-  export interface AuthService { }
-  export class AwsAuthService { }
-}
