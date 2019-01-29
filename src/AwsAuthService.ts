d97fdec05e9deffd8ed67e4770d9c62c1afba110
Daniel Persson <bighatsmallfeet@gmail.com>
2019-01-29 21:21:15 +0100

    fixes

--- src/AwsAuthService.ts
+++
@@ -1,7 +0,0 @@
-import { AuthService } from './AuthService';
-
-export class AwsAuthService implements AuthService {
-  autoAuthorizeNames(name1: string, name2: string): string {
-    return `Authorized: ${name1} and ${name2}`;
-  }
-}
