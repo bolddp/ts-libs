0f18e201e9b4ccf00b53132ac97fe3c5cb366281
Daniel Persson <bighatsmallfeet@gmail.com>
2019-01-29 22:06:39 +0100

    testing hook

--- lib/AuthService.d.ts
+++
@@ -1,3 +0,0 @@
-export interface AuthService {
-    autoAuthorizeNames(name1: string, name2: string): string;
-}
--- lib/AwsAuthService.d.ts
+++
@@ -1,4 +0,0 @@
-import { AuthService } from './AuthService';
-export declare class AwsAuthService implements AuthService {
-    autoAuthorizeNames(name1: string, name2: string): string;
-}
